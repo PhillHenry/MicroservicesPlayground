31e870c2250d5e99de372bc81f72ccff7e12731a
PhillHenry <PhillHenry@gmail.com>
2025-04-23 10:32:47 +0100

    Dependency issue. First stab at a REST endpoint.

---
+++ rest_service/src/main/java/uk/co/odinconsultants/microservices/rest/SmokeResource.java
@@ -0,0 +1,14 @@
+package uk.co.odinconsultants.microservices.rest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class SmokeResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
